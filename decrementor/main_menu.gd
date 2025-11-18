b542dc63fdd9dbf6a7a7708f9c6fff1f2ae11229
Nj3125 <nicholas_johnson5@student.uml.edu>
2025-11-18 01:56:19 -0500

    Added title to tile screen

---
+++ decrementor/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+func _ready():
+	# Gets the Buttons and attaches the _pressed functions to them (this means the functions fire on press):
+	get_node("Button Box (VBoxContainer)/Play").pressed.connect(_on_play_pressed)
+	get_node("Button Box (VBoxContainer)/Settings").pressed.connect(_on_settings_pressed)
+	get_node("Button Box (VBoxContainer)/Quit").pressed.connect(_on_quit_pressed)
+	
+func _on_play_pressed():
+	# Changes scene to main scene:
+	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
+	
+func _on_settings_pressed():
+	# Write code for settings panel here!
+	return
+	
+func _on_quit_pressed():
+	# Quits program:
+	get_tree().quit()
