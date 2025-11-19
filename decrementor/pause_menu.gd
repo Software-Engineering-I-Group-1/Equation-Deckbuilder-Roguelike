3474f735515932fa18e0560087367e8e9dbc1ea0
Nj3125 <nicholas_johnson5@student.uml.edu>
2025-11-19 01:31:16 -0500

    Added pause menu

--- decrementor/pause_menu.gd
+++ decrementor/pause_menu.gd
@@ -3,14 +3,20 @@ extends Control
 func _ready():
 	$"Settings (Panel)".visible = false;
 	# Gets the Buttons and attaches the _pressed functions to them (this means the functions fire on press):
-	get_node("Button Box (VBoxContainer)/Play").pressed.connect(_on_play_pressed)
+	get_node("Button Box (VBoxContainer)/Resume").pressed.connect(_on_resume_pressed)
 	get_node("Button Box (VBoxContainer)/Settings").pressed.connect(_on_settings_pressed)
 	get_node("Settings (Panel)/Back").pressed.connect(_on_back_pressed)
 	get_node("Button Box (VBoxContainer)/Quit").pressed.connect(_on_quit_pressed)
 	
-func _on_play_pressed():
-	# Changes scene to main scene:
-	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
+func _input(event): 
+	if event.is_action_pressed("ui_pause"): toggle_pause_menu()
+	
+func toggle_pause_menu(): 
+	visible = not visible
+	
+func _on_resume_pressed():
+	visible = false
+	get_tree().paused = false
 	
 func _on_settings_pressed():
 	$"Settings (Panel)".visible = true;
