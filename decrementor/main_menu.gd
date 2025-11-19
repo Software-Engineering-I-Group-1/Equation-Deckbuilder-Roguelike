e7dff0e3d461b8914f017f85ce03cde7bfecaf61
Nj3125 <nicholas_johnson5@student.uml.edu>
2025-11-18 22:26:00 -0500

    Added what should be neccesary to host on the Github page

--- decrementor/main_menu.gd
+++ decrementor/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 func _ready():
+	$"Settings (Panel)".visible = false;
 	# Gets the Buttons and attaches the _pressed functions to them (this means the functions fire on press):
 	get_node("Button Box (VBoxContainer)/Play").pressed.connect(_on_play_pressed)
 	get_node("Button Box (VBoxContainer)/Settings").pressed.connect(_on_settings_pressed)
@@ -11,7 +12,7 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
 	
 func _on_settings_pressed():
-	# Write code for settings panel here!
+	$"Settings (Panel)".visible = true;
 	return
 	
 func _on_quit_pressed():
