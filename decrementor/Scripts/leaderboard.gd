ee6747ed3e362d0f6ed8417b3ded8f3f5bccb933
Phykio <naingp8705@gmail.com>
2025-12-02 22:57:15 -0500

    Leaderboard Changes

--- decrementor/Scripts/leaderboard.gd
+++ decrementor/Scripts/leaderboard.gd
@@ -3,48 +3,34 @@ extends GridContainer
 var player_list_with_pos = []
 
 func _ready() -> void:
+	columns = 3
+	add_theme_constant_override("h_separation", 125)
+	add_theme_constant_override("v_separation", 15)
+	
 	await get_tree().process_frame
-	var sw_result: Dictionary = await SilentWolf.Scores.get_scores(0).sw_get_scores_complete
+	var sw_result: Dictionary = await SilentWolf.Scores.get_scores().sw_get_scores_complete
 	player_list_with_pos = sort_players_and_add_position(SilentWolf.Scores.scores)
 	add_player_to_grid(player_list_with_pos)
 	
 func add_player_to_grid(player_list):
-	var pos_vbox = VBoxContainer.new()
-	var name_vbox = VBoxContainer.new()
-	var score_vbox = VBoxContainer.new()
-	
 	for score_data in player_list_with_pos:
 		var pos_label = Label.new()
 		pos_label.text = str(score_data["position"])
-		pos_label.show()
-		pos_vbox.add_child(pos_label)
-	add_child(pos_vbox)
-	
-	for score_data in player_list_with_pos:
+		add_child(pos_label)
+		
 		var name_label = Label.new()
 		name_label.text = str(score_data["player_name"])
-		if name_label.text == "Donald Trump":
-			name_label.text = "Margot Robbie"
-		if name_label.text == "Epstein":
-			name_label.text = "Aespa Karina"
-		if name_label.text == "Bill Clinton":
-			name_label.text = "Taylor Swift"
-		if name_label.text == "t":
-			name_label.text = "Senny Lu"
-		name_label.show()
-		name_vbox.add_child(name_label)
-	add_child(name_vbox)
-	
-	for score_data in player_list_with_pos:
+		add_child(name_label)
+		
 		var score_label = Label.new()
 		score_label.text = str(score_data["score"])
-		score_label.show()
-		score_vbox.add_child(score_label)
-	add_child(score_vbox)
+
+		add_child(score_label)
 	
 func sort_players_and_add_position(player_list):
-	var position = 1
+	player_list.sort_custom(func(a, b): return a["score"] > b["score"])
 	
+	var position = 1
 	for player in player_list:
 		player["position"] = position
 		position += 1
