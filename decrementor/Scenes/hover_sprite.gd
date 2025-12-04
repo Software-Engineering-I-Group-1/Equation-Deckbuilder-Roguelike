21570297990014fb01d52fe062cda498d00e4a8f
Wixisiz <SennyLu101@gmail.com>
2025-12-04 12:25:21 -0500

    commented out console log messages

--- decrementor/Scenes/hover_sprite.gd
+++ decrementor/Scenes/hover_sprite.gd
@@ -1,8 +1,8 @@
 extends Sprite2D
 
 func _on_card_mouse_entered():
-	print("Mouse entered!")
+	#print("Mouse entered!")
 	self.visible = true
 func _on_card_mouse_exited():
-	print("Mouse exited!")
+	#print("Mouse exited!")
 	self.visible = false
